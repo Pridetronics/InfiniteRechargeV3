e9b338d82874094d3a0b338aea5258a1a0bb7a93
StevenWilli60 <tretherager@gmail.com>
2020-02-01 14:10:40 -0700

    Construction of the climb system

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -8,14 +8,45 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+
 
 public class Climb extends SubsystemBase {
   /**
    * Creates a new Climb.
    */
-  public Climb() {
+
+    public CANSparkMax raiseClimbMotor;
+    public Talon telescopicClimbMotor;
+
+    public Joystick shooterGamepad;
+    public JoystickButton raiseTelescopic;
+    public JoystickButton descendTelescopic;
+    public JoystickButton sequenceClimbButton;
+
+  public Climb(Joystick joystickShooter) {
     //Lifts up, grabs bar, pulls self up
+
+    raiseClimbMotor = new CANSparkMax(3, MotorType.kBrushed);
+    telescopicClimbMotor = new Talon(3);
+
+    shooterGamepad = new Joystick(1);
+
+    raiseTelescopic = new JoystickButton(shooterGamepad, 6);
+    descendTelescopic = new JoystickButton(shooterGamepad, 5);
+    sequenceClimbButton = new JoystickButton(shooterGamepad, 3);
+  
+    shooterGamepad = joystickShooter;
+
+
   }
+
 
   @Override
   public void periodic() {
